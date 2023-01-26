468b7d0f53d9d94af86e4aeebe4b26e7c2c0e66d
teejay128 <104542152+Teejay128@users.noreply.github.com>
2023-01-26 16:24:30 +0100

    properly working handlebar templates

--- index.js
+++ index.js
@@ -1,17 +1,53 @@
-const express = require('express');
-const exphbs = require('express-handlebars');
-const path = require('path');
+const express = require('express')
+const { engine } = require('express-handlebars')
 
-const app = express();
+const app = express()
+const port = process.env.PORT || 3000
 
-app.set('views', path.join(__dirname, 'views'));
-app.engine('handlebars', exphbs({ defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
+app.engine('hbs', engine({
+    layoutsDir: `${__dirname}/views/layouts`,
+    extname: 'hbs',
+    defaultLayout: 'index',
+    partialsDir: `${__dirname}/views/partials`
+}))
+app.set('view engine', 'hbs')
+app.set('views', './views');
+
+app.use(express.static('public'))
+
+const fakeApi = () => {
+    return[
+        {
+            name: "Chainsaw",
+            holder: "Denji"
+        },
+        {
+            name: "Blood",
+            holder: "Power"
+        },
+        {
+            name: "Future",
+            holder: "Aki"
+        },
+        {
+            name: "Collosal",
+            holder: "Armin"
+        },
+        {
+            name: "Control",
+            holder: "Makima"
+        },
+        {
+            name: "Jaw",
+            holder: "Falco"
+        },
+    ]
+}
 
 app.get('/', (req, res) => {
-    res.render('home')
+    res.render('main', { layout: 'index', jinjurikis: fakeApi()})
 })
 
-app.listen(3000, () => {
+app.listen(3000, (req, res) => {
     console.log('Server running on port 3000')
 })
