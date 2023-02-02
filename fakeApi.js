41cbe2401c77aedb4fd7e21101604c6cff6e56aa
teejay128 <104542152+Teejay128@users.noreply.github.com>
2023-02-02 18:58:07 +0100

    added the tests for the fakeApi functions

--- fakeApi.js
+++ fakeApi.js
@@ -8,28 +8,72 @@ function getPost(i){
     return fakeDb[i - 1]
 }
 
-function newPost(author, content){
+function createPost(author, content){
     fakeDb.push({
         author,
         content
     })
 }
 
-// function cutPost(i) {
-//     fakeDb.splice(i - 1, 1)
-// }
-// cutPost(2)
+function deletePost(i) {
+    fakeDb.splice(i - 1, 1)
+}
 
-// newPost("Mr meat", "The sea is red, IMO")
-// newPost("Mr fish", "The sea is blue, IMO")
-// newPost("Mr egg", "The sea is yellow, IMO")
+function updatePost(i, author, content) {
+    fakeDb[i - 1].author = author
+    fakeDb[i - 1].content = content
+}
 
-// console.log(allPosts())
-// console.log(allPosts())
-// console.log(getPost(2))
+// TESTING THE FUNCTIONS
+// Should return an empty database
+console.log(allPosts())
+// []
+
+// Should add 3 posts to the database
+createPost("Mr meat", "The sea is red, IMO")
+createPost("Mr fish", "The sea is blue, IMO")
+createPost("Mr egg", "The sea is yellow, IMO")
+
+// Should return the database with 3 posts
+console.log(allPosts())
+
+// [
+//     { author: 'Mr meat', content: 'The sea is red, IMO' },
+//     { author: 'Mr fish', content: 'The sea is blue, IMO' },
+//     { author: 'Mr egg', content: 'The sea is yellow, IMO' }
+// ]
+
+// Should return the second post in the database\
+console.log(getPost(2))
+
+//  { author: 'Mr fish', content: 'The sea is blue, IMO' }
+
+// Should delete the first post
+deletePost(1)
+
+// Should return the entire database
+console.log(allPosts())
+
+// [
+//     { author: 'Mr fish', content: 'The sea is blue, IMO' },
+//     { author: 'Mr egg', content: 'The sea is yellow, IMO' }
+// ]
+
+// Should update the second post
+updatePost(2, "Miss fish", "I don't care about the sea")
+
+// Should return the entire database
+console.log(allPosts())
+
+// [
+//     { author: 'Mr fish', content: 'The sea is blue, IMO' },
+//     { author: 'Miss fish', content: "I don't care about the sea" }
+// ]
 
 module.exports = {
     allPosts,
     getPost,
-    newPost
+    createPost,
+    deletePost,
+    updatePost
 }
